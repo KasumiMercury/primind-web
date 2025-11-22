5d2046c421c281d165862030042a3963ba75c1a2
KasumiMercury <naoki_shira@outlook.jp>
2025-11-23 00:06:01 +0900

    chore: setup Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,27 @@
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react-vite";
+
+import "../app/app.css";
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+    decorators: [
+        withThemeByDataAttribute<ReactRenderer>({
+            themes: {
+                light: "rosepine-dawn",
+                dark: "rosepine-moon",
+            },
+            defaultTheme: "light",
+            attributeName: "data-theme",
+        }),
+    ],
+};
+
+export default preview;
