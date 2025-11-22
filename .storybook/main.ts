2af2379e76e2539fa8e43dbf34883d1b3923d5c6
KasumiMercury <naoki_shira@outlook.jp>
2025-11-23 00:56:47 +0900

    style: formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,7 @@ const config: StorybookConfig = {
         "@storybook/addon-onboarding",
         "@storybook/addon-a11y",
         "@storybook/addon-vitest",
-        "@storybook/addon-themes"
+        "@storybook/addon-themes",
     ],
     framework: {
         name: "@storybook/react-vite",
