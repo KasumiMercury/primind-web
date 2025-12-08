4c50ef8a473cbfe44ccefe301cb2bb554b97b192
KasumiMercury <naoki_shira@outlook.jp>
2025-12-08 14:57:04 +0900

    feat(task): add quick edit feature to welcome page

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,6 +5,7 @@ import { v7 as uuidv7 } from "uuid";
 import { LoginDialog } from "~/auth/login-dialog";
 import { Button } from "~/components/ui/button";
 import { OperationArea } from "~/task/operation-area";
+import { QuickEdit } from "~/task/quick-edit";
 import { createTaskFormData } from "~/task/task-form-data";
 import type { TaskTypeKey } from "~/task/task-type-items";
 
@@ -45,9 +46,9 @@ export function Welcome() {
     return (
         <>
             <main className="flex items-center justify-center pt-8">
-                <div className="flex min-h-0 flex-1 flex-col items-center gap-16">
+                <div className="flex min-h-0 flex-1 flex-col items-center gap-4 px-4">
                     <header className="w-full max-w-4xl">
-                        <div className="flex w-full items-center justify-end px-4">
+                        <div className="flex w-full items-center justify-end">
                             <Button
                                 onClick={() =>
                                     setSearchParams({ login: "true" })
@@ -59,7 +60,16 @@ export function Welcome() {
                             </Button>
                         </div>
                     </header>
-                    <div className="w-full max-w-[300px] space-y-6 px-4"></div>
+                    {latestTask && (
+                        <div className="w-full max-w-3xl rounded-md border-2 border-secondary px-2 py-4">
+                            <QuickEdit
+                                key={latestTask.taskId}
+                                className="w-full"
+                                taskId={latestTask.taskId}
+                                taskTypeKey={latestTask.taskTypeKey}
+                            />
+                        </div>
+                    )}
                 </div>
                 <div className="absolute inset-x-0 bottom-0 flex items-center justify-center p-4">
                     <OperationArea
