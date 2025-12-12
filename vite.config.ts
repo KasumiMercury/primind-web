6bc41bc5a7da8e5a643647908e614201464b9bbc
KasumiMercury <naoki_shira@outlook.jp>
2025-12-12 22:35:15 +0900

    chore: setup react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const isStorybook = process.argv[1]?.includes("storybook");
 
 export default defineConfig({
-    plugins: [tailwindcss(), !isStorybook && reactRouter(), tsconfigPaths()],
+    plugins: [
+        tailwindcss(),
+        !isStorybook && reactRouter(),
+        tsconfigPaths(),
+        babel({
+            filter: /\.[jt]sx?$/,
+            babelConfig: {
+                presets: ["@babel/preset-typescript"],
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        }),
+    ],
     server: {
         host: "0.0.0.0",
         port: 5173,
