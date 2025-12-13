bb916ddc0ed211a259fd6169799e01d4efb6ae2d
KasumiMercury <naoki_shira@outlook.jp>
2025-12-13 23:04:10 +0900

    refactor: refactor UI components to use react-aria-components

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -1,13 +1,19 @@
-import type * as React from "react";
+import {
+    TextArea as AriaTextArea,
+    type TextAreaProps,
+} from "react-aria-components";
 
 import { cn } from "~/lib/utils";
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+function Textarea({ className, ...props }: TextAreaProps) {
     return (
-        <textarea
+        <AriaTextArea
             data-slot="textarea"
             className={cn(
-                "field-sizing-content flex min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
+                "field-sizing-content flex min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground md:text-sm dark:bg-input/30",
+                "data-focus-visible:border-ring data-focus-visible:ring-4 data-focus-visible:ring-ring/50",
+                "data-invalid:border-destructive data-invalid:ring-destructive/20 dark:data-invalid:ring-destructive/40",
+                "data-disabled:cursor-not-allowed data-disabled:opacity-50",
                 className,
             )}
             {...props}
