7def71f20255f648dea3e5157ab0fd8fd43f08af
KasumiMercury <naoki_shira@outlook.jp>
2025-12-16 23:16:08 +0900

    style: formatting

--- app/features/task/components/operation-area.tsx
+++ app/features/task/components/operation-area.tsx
@@ -224,7 +224,7 @@ export function OperationArea({
                     />
                 </div>
             ) : null}
-            <div className="-translate-x-1/2 pointer-events-none absolute bottom-2 left-1/2 z-20">
+            <div className="pointer-events-none·absolute·bottom-2·left-1/2·z-20·-translate-x-1/2">
                 <OperationIndicator
                     itemCount={itemsCount}
                     selectedIndex={selectedIndex}
