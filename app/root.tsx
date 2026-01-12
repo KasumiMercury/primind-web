c1cb354910f8e7a9603e9f316bdf7e1269c8b76d
KasumiMercury <naoki_shira@outlook.jp>
2026-01-12 22:13:43 +0900

    feat: setup favicon links and meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -27,6 +27,20 @@ export const links: Route.LinksFunction = () => [
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
     },
+    {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -48,6 +62,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
+                <meta name="apple-mobile-web-app-title" content="PriMind" />
                 <Meta />
                 <Links />
             </head>
